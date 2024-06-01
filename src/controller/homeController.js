529bbea6f8c4aa12453d49099d09f2916629c9a7
Tuan Anh <eszss56@gmail.com>
2024-06-01 20:23:08 +0700

    Divide Route for website

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -8,33 +8,53 @@ const handleScheduler=(req,res)=>{
     return res.render('scheduler.ejs')
 }
 
-const handleTeam=(req,res)=>{
-    var clubs=[{
-        name:'Tun',
-        wins:1,
-        draws:1,
-        losts:1,
-        goalsFor:1,
-        goalsAgainst:1,
-        goalsDiff:1,
-        point:1
-    }]
-    return res.render('team.ejs',{clubs})
+const handleRanking=(req,res)=>{
+    return res.render('ranking.ejs')
 }
 
-const handleAddUser=(req,res)=>{
-    let userList=userService.getUserList()
-    return res.render('user-add.ejs',{userList})
+const handleTeams=(req,res)=>{
+    return res.render('teams.ejs')
+}
+
+const handlePlayers=(req,res)=>{
+    return res.render('players.ejs')
+}
+
+const handleInformation=(req,res)=>{
+    return res.render('information.ejs')
 }
 
 
-const handleCreateUser=(req,res)=>{
-    let email=req.body.email
-    let password=req.body.password
-    let username=req.body.username
-
-    return res.redirect('/users/')
-}
+// 
 module.exports={
-    handleHomePage,handleScheduler,handleTeam,handleAddUser,handleCreateUser
+    handleHomePage,handleScheduler,handleRanking,handleTeams,handlePlayers,handleInformation
 }
+
+
+// const handleTeam=(req,res)=>{
+    //     var clubs=[{
+    //         name:'Tun',
+    //         wins:1,
+    //         draws:1,
+    //         losts:1,
+    //         goalsFor:1,
+    //         goalsAgainst:1,
+    //         goalsDiff:1,
+    //         point:1
+    //     }]
+    //     return res.render('team.ejs',{clubs})
+    // }
+    
+    // const handleAddUser=(req,res)=>{
+    //     let userList=userService.getUserList()
+    //     return res.render('user-add.ejs',{userList})
+    // }
+    
+    
+    // const handleCreateUser=(req,res)=>{
+    //     let email=req.body.email
+    //     let password=req.body.password
+    //     let username=req.body.username
+    
+    //     return res.redirect('/users/')
+    // }
